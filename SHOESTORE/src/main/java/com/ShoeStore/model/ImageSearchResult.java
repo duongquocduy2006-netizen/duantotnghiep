7216a89f246ca26a8b90b81c6b5bebd56026ceac
thnga96-oss <thuynga.962006@gmail.com>
2026-07-12 20:20:12 +0700

    Resolve merge conflicts: keep local changes

---
+++ SHOESTORE/src/main/java/com/ShoeStore/model/ImageSearchResult.java
@@ -0,0 +1,24 @@
+package com.ShoeStore.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+public class ImageSearchResult {
+    private String brand;
+    private String category;
+    private String color;
+
+    public String getBrand() {
+        return brand != null ? brand : "";
+    }
+
+    public String getCategory() {
+        return category != null ? category : "";
+    }
+
+    public String getColor() {
+        return color != null ? color : "";
+    }
+}
